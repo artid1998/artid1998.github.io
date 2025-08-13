baf80858a5e0afe6f28a1d058917b57bdff0494d
mun940 <qaaz345@naver.com>
2025-08-13 16:07:58 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const portfolioData = [
+  {
+    category: "주택",
+    title: "파장동 주택공사", 
+    desc: "파장동에 있는 주택.",
+    image: "shop1.jpg"
+  },
+  {
+    category: "상가",
+    title: "논현 주얼리샵",
+    desc: "서울 논현에 위치한 주얼리샵.",
+    image: "shop2.jpg"
+  }
+];
